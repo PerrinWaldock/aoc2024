41c9c2a49ffc6c60c1d02712c58f2b349d89e691
PerrinWaldock <PerrinWaldock@users.noreply.github.com>
2024-12-05 22:30:58 -0800

    adding day 6

---
+++ 06/d6p1.py
@@ -0,0 +1,50 @@
+import numpy as np
+from collections import deque
+import re
+import os
+
+filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input.txt")
+#filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "sample.txt")
+
+lines = deque()
+with open(filename) as f:
+    for line in f:
+        lines.append(list(line.replace("\n", "")))
+arr = np.array(lines)
+
+directions = deque()
+positions = deque()
+start = tuple(np.argwhere(arr == "^")[0])
+positions.append(start)
+directions.append(0)
+
+def inBounds(x, y):
+    return 0 <= x < np.shape(arr)[0] and 0 <= y <= np.shape(arr)[1]
+
+def getNext(x, y, d):
+    if d == 0:
+        xnew = x-1
+        ynew = y
+    elif d == 1:
+        ynew = y + 1
+        xnew = x
+    elif d == 2:
+        xnew = x+1 
+        ynew = y
+    elif d == 3:
+        ynew = y - 1
+        xnew = x
+    
+    if inBounds(xnew,ynew) and arr[xnew,ynew] == "#":
+        d = (d + 1)%4
+        return getNext(x,y,d)
+    else:
+        return xnew,ynew,d
+
+while inBounds(*positions[-1]):
+    x,y,d = getNext(*positions[-1],directions[-1])
+    positions.append((x,y))
+    directions.append(d)
+
+landedPositions = list(set(positions))
+print(len(landedPositions) - 1)
---
+++ 06/d6p2.py
@@ -0,0 +1,64 @@
+import numpy as np
+from collections import deque
+import re
+import os
+from tqdm import tqdm
+
+filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input.txt")
+#filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "sample.txt")
+
+lines = deque()
+with open(filename) as f:
+	for line in f:
+		lines.append(list(line.replace("\n", "")))
+arr = np.array(lines)
+
+start = tuple(np.argwhere(arr == "^")[0])
+startdir = 0
+startposition = (*start, startdir)
+
+def inBounds(x, y, d):
+	return 0 <= x < np.shape(arr)[0] and 0 <= y < np.shape(arr)[1]
+
+def causesLoop(testarr):
+	currentposition = startposition
+	pandd = set()
+	pandd.add(currentposition)
+
+	def getNext(x, y, d):
+		if d == 0:
+			xnew = x-1
+			ynew = y
+		elif d == 1:
+			ynew = y + 1
+			xnew = x
+		elif d == 2:
+			xnew = x+1 
+			ynew = y
+		elif d == 3:
+			ynew = y - 1
+			xnew = x
+		
+		if inBounds(xnew,ynew, d) and testarr[xnew,ynew] == "#":
+			d = (d + 1)%4
+			return getNext(x,y,d)
+		else:
+			return xnew,ynew,d
+
+	while inBounds(*currentposition):
+		currentposition = getNext(*currentposition)
+		if currentposition in pandd:
+			return True, pandd
+		pandd.add((currentposition))
+	return False, pandd
+
+_, positionsToTest = causesLoop(arr)
+positionsToTest = list(set([p[0:2] for p in positionsToTest if p != startposition and inBounds(*p)]))
+
+validPositions = deque()
+for p in tqdm(positionsToTest):
+    newarr = arr.copy()
+    newarr[*p] = "#"
+    if causesLoop(newarr)[0]:
+        validPositions.append(p)
+print(len(set(validPositions)))
